4164a44589253465d5b647bb94837562f307b421
windowsflytheac <88598431+windowsflytheac@users.noreply.github.com>
2025-09-15 20:03:15 -0700

    Update game.js

--- src/client/game.js
+++ src/client/game.js
@@ -2,10 +2,10 @@ import { Engine } from './engine.js';
 
 const engine = new Engine({ container: document.body });
 
-// Set your placeholder skybox image
+// Single-texture skybox
 engine.setSkybox('textures/fixed_skybox_placeholder.png');
 
-// Add cubes for testing
+// Test cubes
 engine.addCube(0, 0.5, -2);
 engine.addCube(2, 0.5, -4);
 engine.addCube(-2, 0.5, -6);
